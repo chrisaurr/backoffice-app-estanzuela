89e548487042c57472800312eb8c9b93df7f3236
Julio Aguilar <jcas3322@gmail.com>
2024-05-08 20:28:04 -0600

    Agregado mapa

--- backoffice/src/main/java/com/umg/backoffice/controller/CasosConstroller.java
+++ backoffice/src/main/java/com/umg/backoffice/controller/CasosConstroller.java
@@ -1,10 +1,12 @@
 package com.umg.backoffice.controller;
 
+import com.umg.backoffice.modelo.entity.Constants;
 import com.umg.backoffice.modelo.entity.Incidente;
 import com.umg.backoffice.service.incidente.service.IncidenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,8 +23,17 @@ public class CasosConstroller {
     public ModelAndView allCases(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/casos/listar");
-        List<Incidente> listadoIncidentes = incidenteService.getAllIncidentes(0);
+        List<Incidente> listadoIncidentes = incidenteService.getAllIncidentes(Constants.ESTADO_ELIMINADO);
         mv.addObject("listadoIncidentes", listadoIncidentes);
+        return mv;
+    }
+
+    @GetMapping("/detalle/{id}")
+    public ModelAndView detalleCase(@PathVariable("id") Long id){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/casos/detalle");
+        Incidente incidente = incidenteService.getIncidenteById(id, Constants.ESTADO_ELIMINADO);
+        mv.addObject("incidente", incidente);
         return mv;
     }
 }
---
+++ backoffice/src/main/java/com/umg/backoffice/modelo/entity/Constants.java
@@ -0,0 +1,9 @@
+package com.umg.backoffice.modelo.entity;
+
+public class Constants {
+
+    public static Integer ESTADO_ELIMINADO = 0;
+    public static Integer ESTADO_SIN_PROCESO = 1;
+    public static Integer ESTADO_SOLUCIONADO = 2;
+    public static Integer ESTADO_NO_APLICA = 3;
+}
